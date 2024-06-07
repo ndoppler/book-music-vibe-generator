39540f65a84a03f85f5f1ea62054634e30d88ca2
Leena <leenacruz@gmail.com>
2024-06-06 17:59:42 -0700

    Logic for fetch test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
 const libraryAPI = "https://openlibrary.org/search.json?title="
+const searchButton = document.getElementById('searchButton');
+
+
+
+function getApi () {
+
+    fetch(libraryAPI)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data)) {
+        console.log(data)
+    }
+}
+
+
+
+
+
+
+
+
+searchButton.addEventListener('click', getApi)
