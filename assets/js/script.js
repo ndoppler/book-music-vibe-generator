f60257e5801613f93dbea1943ccaa3b42c3311e8
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-06-11 19:27:55 -0500

    addition of 4 spotify playlist results and formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // JavaScript JQuery Elements
 spotifyResultsEl = $('#spotifyResults')
+topFourResultsEl = $('#topFourResults')
 
 // Spotify Credentials
 const clientId = '9fa2e110194042238f5becb0c3425fc1';
@@ -33,32 +34,86 @@ function getPlaylist(access_token, search) {
       spotifyResultsHeader = $('<h2>')
         .text = 'Spotify Results'
 
-// Generation of Playlist One Elements for Playlist One Card
+      // Generation of Playlist One Elements for Playlist One Card
       playlistOneCard = $('<div>')
-        .addClass("card m-5 column is-one-quarter")
+        .addClass("card column")
+      playlistTwoCard = $('<div>')
+        .addClass("card column")
+      playlistThreeCard = $('<div>')
+        .addClass("card column")
+      playlistFourCard = $('<div>')
+        .addClass("card column")
 
       playlistOneCardContent = $('<div>')
+        .addClass("card-content has-background-primary-dark ")
+      playlistTwoCardContent = $('<div>')
+        .addClass("card-content has-background-primary-dark ")
+      playlistThreeCardContent = $('<div>')
+        .addClass("card-content has-background-primary-dark ")
+      playlistFourCardContent = $('<div>')
         .addClass("card-content has-background-primary-dark ")
 
       playlistOneMedia = $('<div>')
         .addClass("media")
+      playlistTwoMedia = $('<div>')
+        .addClass("media")
+      playlistThreeMedia = $('<div>')
+        .addClass("media")
+      playlistFourMedia = $('<div>')
+        .addClass("media")
 
       playlistOneMediaLeft = $('<div>')
         .addClass("media-left")
+      playlistTwoMediaLeft = $('<div>')
+        .addClass("media-left")
+      playlistThreeMediaLeft = $('<div>')
+        .addClass("media-left")
+      playlistFourMediaLeft = $('<div>')
+        .addClass("media-left")
 
       playlistOneFigure = $('<div>')
+        .addClass("image is-48x48")
+      playlistTwoFigure = $('<div>')
+        .addClass("image is-48x48")
+      playlistThreeFigure = $('<div>')
+        .addClass("image is-48x48")
+      playlistFourFigure = $('<div>')
         .addClass("image is-48x48")
 
       playlistOneImage = $('<img>')
         .addClass('image is-rounded')
         .attr('src', `${data.playlists.items[0].images[0].url}`)
+      playlistTwoImage = $('<img>')
+        .addClass('image is-rounded')
+        .attr('src', `${data.playlists.items[1].images[0].url}`)
+      playlistThreeImage = $('<img>')
+        .addClass('image is-rounded')
+        .attr('src', `${data.playlists.items[2].images[0].url}`)
+      playlistFourImage = $('<img>')
+        .addClass('image is-rounded')
+        .attr('src', `${data.playlists.items[3].images[0].url}`)
 
       playlistOneMediaContent = $('<div>')
+        .addClass("media-content")
+      playlistTwoMediaContent = $('<div>')
+        .addClass("media-content")
+      playlistThreeMediaContent = $('<div>')
+        .addClass("media-content")
+      playlistFourMediaContent = $('<div>')
         .addClass("media-content")
 
       playlistOneHeader = $('<p>')
         .addClass("title is-4")
         .text(`${data.playlists.items[0].name}`)
+      playlistTwoHeader = $('<p>')
+        .addClass("title is-4")
+        .text(`${data.playlists.items[1].name}`)
+      playlistThreeHeader = $('<p>')
+        .addClass("title is-4")
+        .text(`${data.playlists.items[2].name}`)
+      playlistFourHeader = $('<p>')
+        .addClass("title is-4")
+        .text(`${data.playlists.items[3].name}`)
 
       playlistOneSubHeader = $('<a>')
         .addClass("subtitle is-6 m-2")
@@ -67,20 +122,73 @@ function getPlaylist(access_token, search) {
           "target": "_blank"
         })
         .text('View Playlist')
+      playlistTwoSubHeader = $('<a>')
+        .addClass("subtitle is-6 m-2")
+        .attr({
+          "href": data.playlists.items[1].external_urls.spotify,
+          "target": "_blank"
+        })
+        .text('View Playlist')
+      playlistThreeSubHeader = $('<a>')
+        .addClass("subtitle is-6 m-2")
+        .attr({
+          "href": data.playlists.items[2].external_urls.spotify,
+          "target": "_blank"
+        })
+        .text('View Playlist')
+      playlistFourSubHeader = $('<a>')
+        .addClass("subtitle is-6 m-2")
+        .attr({
+          "href": data.playlists.items[3].external_urls.spotify,
+          "target": "_blank"
+        })
+        .text('View Playlist')
 
       // Creation of Spotify Results Header --Still needs Bulma Formatting
       spotifyResultsEl.append(spotifyResultsHeader)
       // Creation of Spotify Playlist 1 Result Card
-      spotifyResultsEl.append(playlistOneCard)
+      topFourResultsEl.append(playlistOneCard)
+      topFourResultsEl.append(playlistTwoCard)
+      topFourResultsEl.append(playlistThreeCard)
+      topFourResultsEl.append(playlistFourCard)
       playlistOneCard.append(playlistOneCardContent)
+      playlistTwoCard.append(playlistTwoCardContent)
+      playlistThreeCard.append(playlistThreeCardContent)
+      playlistFourCard.append(playlistFourCardContent)
       playlistOneCardContent.append(playlistOneMedia)
+      playlistTwoCardContent.append(playlistTwoMedia)
+      playlistThreeCardContent.append(playlistThreeMedia)
+      playlistFourCardContent.append(playlistFourMedia)
+
       playlistOneMedia.append(playlistOneMediaLeft)
+      playlistTwoMedia.append(playlistTwoMediaLeft)
+      playlistThreeMedia.append(playlistThreeMediaLeft)
+      playlistFourMedia.append(playlistFourMediaLeft)
+
       playlistOneMedia.append(playlistOneMediaContent)
+      playlistTwoMedia.append(playlistTwoMediaContent)
+      playlistThreeMedia.append(playlistThreeMediaContent)
+      playlistFourMedia.append(playlistFourMediaContent)
+
       playlistOneMediaLeft.append(playlistOneFigure)
+      playlistTwoMediaLeft.append(playlistTwoFigure)
+      playlistThreeMediaLeft.append(playlistThreeFigure)
+      playlistFourMediaLeft.append(playlistFourFigure)
+
       playlistOneFigure.append(playlistOneImage)
+      playlistTwoFigure.append(playlistTwoImage)
+      playlistThreeFigure.append(playlistThreeImage)
+      playlistFourFigure.append(playlistFourImage)
+
       playlistOneMediaContent.append(playlistOneHeader)
+      playlistTwoMediaContent.append(playlistTwoHeader)
+      playlistThreeMediaContent.append(playlistThreeHeader)
+      playlistFourMediaContent.append(playlistFourHeader)
+
       playlistOneMediaContent.append(playlistOneSubHeader)
-      playlistOneMediaContent.append(playlistOneTrackModal)
+      playlistTwoMediaContent.append(playlistTwoSubHeader)
+      playlistThreeMediaContent.append(playlistThreeSubHeader)
+      playlistFourMediaContent.append(playlistFourSubHeader)
     });
 }
 
